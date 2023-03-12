a9258eb8fa2087a4b7925a094a52da5b65dc1fe6
João de Macedo Borges <hello@jbbmb.com>
2023-03-12 04:58:50 +0000

    Spring 2023 Revision

--- static/js/main.js
+++ static/js/main.js
@@ -2,37 +2,37 @@
 /** Initializes variables */
 
 const phrases = [
-    getGreeting(),
-    'Welcome to the virtual corner of',
-    'this male Homo Sapiens specimen',
-    'born early April of 1997 in Portugal',
-    'who is passionate about technology',
-    'prefers a nice gin tonic over beer',
-    'keeps changing his wallpapers',
-    'with the World on his bucket list',
-    'and that thanks You for visiting.'
-],
+        getGreeting(),
+        'Welcome to the virtual corner of',
+        'this male Homo Sapiens specimen',
+        'born early April of 1997 in Portugal',
+        'who is passionate about technology',
+        'prefers a nice gin tonic over beer',
+        'keeps changing his wallpapers',
+        'with the World on his bucket list',
+        'and that thanks You for visiting.'
+    ],
     contacts = [{
-        type: "item",
-        label: "Mail",
-        callback: () => {
-            gateway(1, "_blank");
+            type: "item",
+            label: "Mail",
+            callback: () => {
+                gateway(1, "_blank");
+            }
+        },
+        {
+            type: "item",
+            label: "Telegram",
+            callback: () => {
+                gateway(3, "_blank");
+            }
         }
-    },
-    {
-        type: "item",
-        label: "Telegram",
-        callback: () => {
-            gateway(3, "_blank");
-        }
-    }
     ],
     context = new Context(".contexted", contacts);
 
 
 /** Triggers on session load */
 
-window.addEventListener('load', function () {
+window.addEventListener('load', function() {
 
 
     /** Pings Analytics */
@@ -81,7 +81,7 @@ window.addEventListener('load', function () {
     var cardContainerHeight = cardContainer.getBoundingClientRect().height * 0.5 + cardContainer.getBoundingClientRect().top,
         cardContainerWidth = cardContainer.getBoundingClientRect().width * 0.5 + cardContainer.getBoundingClientRect().left;
 
-    window.addEventListener('resize', function () {
+    window.addEventListener('resize', function() {
         cardContainerHeight = cardContainer.getBoundingClientRect().height * 0.5 + cardContainer.getBoundingClientRect().top;
         cardContainerWidth = cardContainer.getBoundingClientRect().width * 0.5 + cardContainer.getBoundingClientRect().left;
     }, true);
@@ -93,18 +93,18 @@ window.addEventListener('load', function () {
 
     /** Activates input event listeners */
 
-    document.querySelectorAll("#main").forEach(function (item) {
-        item.addEventListener("mousemove", function (e) {
+    document.querySelectorAll("#main").forEach(function(item) {
+        item.addEventListener("mousemove", function(e) {
             getSchwifty(cardContainer, e.clientX, e.clientY);
         });
-        item.addEventListener("mouseout", function () {
+        item.addEventListener("mouseout", function() {
             getSchwifty(cardContainer, cardContainerWidth, cardContainerHeight);
             document.querySelector('#description').style.display = 'none';
             document.querySelector('#greeting').style.display = 'block';
         });
     });
 
-    document.querySelector('#main').addEventListener('touchmove', function (e) {
+    document.querySelector('#main').addEventListener('touchmove', function(e) {
         e.preventDefault();
         const touch = e.targetTouches[0];
         if (touch) {
@@ -112,11 +112,11 @@ window.addEventListener('load', function () {
         }
     });
 
-    document.querySelector('#wrapper').addEventListener('touchmove', function (e) {
+    document.querySelector('#wrapper').addEventListener('touchmove', function(e) {
         e.preventDefault();
     });
 
-    document.addEventListener('touchend', function (e) {
+    document.addEventListener('touchend', function(e) {
         e.stopPropagation();
         getSchwifty(cardContainer, cardContainerWidth, cardContainerHeight);
         document.querySelector('#description').style.display = 'none';
@@ -125,31 +125,31 @@ window.addEventListener('load', function () {
 
     document.addEventListener('contextmenu', event => event.preventDefault());
 
-    document.querySelectorAll(".icon").forEach(function (icon) {
-        icon.addEventListener("mousemove", function () {
+    document.querySelectorAll(".icon").forEach(function(icon) {
+        icon.addEventListener("mousemove", function() {
             document.querySelector('#description').innerHTML = icon.getAttribute('alt');
             document.querySelector('#greeting').style.display = 'none';
             document.querySelector('#description').style.display = 'block';
         });
-        icon.addEventListener("mouseout", function () {
+        icon.addEventListener("mouseout", function() {
             document.querySelector('#description').style.display = 'none';
             document.querySelector('#greeting').style.display = 'block';
         });
     });
 
-    document.querySelectorAll(".direct").forEach(function (icon) {
-        icon.addEventListener("click", function () {
+    document.querySelectorAll(".direct").forEach(function(icon) {
+        icon.addEventListener("click", function() {
             gateway(parseInt(icon.getAttribute('id')), "_blank");
         });
     });
 
-    document.querySelectorAll(".modern_context_js_outer").forEach(function (item) {
-        item.addEventListener("mousemove", function (e) {
+    document.querySelectorAll(".modern_context_js_outer").forEach(function(item) {
+        item.addEventListener("mousemove", function(e) {
             getSchwifty(cardContainer, e.clientX, e.clientY);
             document.querySelector('#greeting').style.display = 'none';
             document.querySelector('#description').style.display = 'block';
         });
-        item.addEventListener("mouseout", function () {
+        item.addEventListener("mouseout", function() {
             getSchwifty(cardContainer, cardContainerWidth, cardContainerHeight);
             document.querySelector('#description').style.display = 'none';
             document.querySelector('#greeting').style.display = 'block';
@@ -169,7 +169,7 @@ function gateway(node, target) {
             });
             navigator.clipboard.writeText("hello@jbbmb.com");
             SnackBar({
-                message: "Mail address copied to clipboard...&nbsp",
+                message: "Address copied to the clipboard!&nbsp",
                 status: "info",
                 position: "tr",
                 fixed: true,
@@ -278,11 +278,11 @@ function reload(node) {
 
 function getGreeting() {
     var goodX = [
-        [0, 5, "good night!"],
-        [6, 11, "good morning!"],
-        [12, 19, "good afternoon!"],
-        [20, 23, "good evening!"]
-    ],
+            [0, 5, "good night!"],
+            [6, 11, "good morning!"],
+            [12, 19, "good afternoon!"],
+            [20, 23, "good evening!"]
+        ],
         hr = new Date().getHours();
     for (var i = 0; i < goodX.length; i++) {
         if (hr >= goodX[i][0] && hr <= goodX[i][1]) {

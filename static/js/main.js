b3770f93d3582839c4494faa768bcf9cc9eb633e
João de Macedo Borges <hello@jbbmb.com>
2024-01-17 18:57:41 +0000

    Winter update reboot

--- static/js/main.js
+++ static/js/main.js
@@ -2,32 +2,55 @@
 /** Initializes variables */
 
 const phrases = [
-        getGreeting(),
-        'Welcome to the virtual corner of',
-        'this male Homo Sapiens specimen',
-        'born early April of 1997 in Portugal',
-        'who is passionate about technology',
-        'prefers a nice gin tonic over beer',
-        'keeps changing his wallpapers',
-        'with the World on his bucket list',
-        'and that thanks You for visiting.'
+        'Homo Sapiens specimen in his 20s.',
+        'Born in northern Portugal, Earth.',
+        'Currently lives in the city of Porto.',
+        'Developer-disguised IAM Architect.',
+        'Studies software and user experience.',
+        'Passionate about personal technology.',
+        'Obsessed with privacy and security.',
+        'Nags companies to delete old accounts.',
+        'Believes AI is nothing to be scared of.',
+        'Trying to leave his own legacy.',
+        'Staying hungry and foolish.',
+        'Can\'t settle on an iPhone wallpaper.',
+        'Hates notifications and texting.',
+        'Atheist second child from a big family.',
+        'Rocks to pop and electronic music.',
+        'Goes for a nice gin and tonic over beer.',
+        'Prefers cats over dogs any day.',
+        'Can speak four languagues fluently.',
+        'Spends his money on trips and burguers.',
+        'Has set foot in eight countries so far.',
+        'Always takes pictures of the sunset.',
+        'Takes care of plants that end up dieing.',
+        'Believes in climate change and vaccines.',
+        'Votes every time there is an election.',
+        'Has built this tiny corner of the web.'
     ],
-    contacts = [{
+    options = [{
+        type: "item",
+        label: "Copyright",
+        callback: () => {
+            gateway(12, "_blank");
+        }
+    },
+    {
             type: "item",
             label: "Privacy Policy",
-            callback: () => {
-                gateway(12, "_blank");
-            }
-        },
-        {
-            type: "item",
-            label: "Version History",
             callback: () => {
                 gateway(13, "_blank");
             }
         },
+        {
+            type: "item",
+            label: "System Status",
+            callback: () => {
+                gateway(14, "_blank");
+            }
+        },
     ],
-    context = new Context(".contexted", contacts);
+    context = new Context(".contexted", options);
 
 
 /** Triggers on session load */
@@ -44,26 +67,35 @@ window.addEventListener('load', function() {
 
     /** Evaluates URL parameters */
 
-    const node = window.location.href.split("#")[1];
+    const node = window.location.href.split("?")[1];
     if (node != null) {
-        document.querySelector('#description').innerHTML = 'Redirecting You...';
-        document.querySelector('#greeting').style.display = 'none';
-        document.querySelector('#description').style.display = 'block';
         setTimeout(() => {
             gateway(parseInt(node), "_self");
         }, 500);
-
     }
 
 
-    /** Removes preload tag */
+    /** Adds greeting to intro */
 
-    document.body.className = document.body.className.replace(/\bis-preload\b/, '');
+    var goodX = [
+        [0, 5, "Good night.<br><bdi>Just scroll.</bdi>"],
+        [6, 11, "Good morning.<br><bdi>Just scroll.</bdi>"],
+        [12, 19, "Good afternoon.<br><bdi>Just scroll.</bdi>"],
+        [20, 23, "Good evening.<br><bdi>Just scroll.</bdi>"]
+    ],
+    hr = new Date().getHours();
+    for (var i = 0; i < goodX.length; i++) {
+        if (hr >= goodX[i][0] && hr <= goodX[i][1]) {
+            console.log("So, we're opening Web Inspector now...?");
+            document.getElementById('greeting').innerHTML = (goodX[i][2]);
+            break;
+        }
+    }
 
 
     /** Activates TextScramble */
 
-    const el = document.querySelector('#greeting'),
+    const el = document.querySelector('#punchline'),
         fx = new TextScramble(el);
     let counter = 0;
     const next = () => {
@@ -78,17 +110,18 @@ window.addEventListener('load', function() {
     /** Animates card with parallax... */
 
     const cardContainer = document.querySelector('#main');
-    var cardContainerHeight = cardContainer.getBoundingClientRect().height * 0.5 + cardContainer.getBoundingClientRect().top,
+    var cardContainerHeight = cardContainer.getBoundingClientRect().height * 0.88,
         cardContainerWidth = cardContainer.getBoundingClientRect().width * 0.5 + cardContainer.getBoundingClientRect().left;
 
     window.addEventListener('resize', function() {
-        cardContainerHeight = cardContainer.getBoundingClientRect().height * 0.5 + cardContainer.getBoundingClientRect().top;
+        cardContainerHeight = cardContainer.getBoundingClientRect().height * 0.88;
         cardContainerWidth = cardContainer.getBoundingClientRect().width * 0.5 + cardContainer.getBoundingClientRect().left;
     }, true);
 
     function getSchwifty(el, positionX, positionY) {
         el.style.setProperty('transform', `rotateX(${(positionY - cardContainerHeight) / 30}deg) rotateY(${((positionX - cardContainerWidth) / 30) * (-1)}deg) translateZ(-15px)`);
-    }
+    }    
+    getSchwifty(cardContainer, cardContainerWidth, cardContainerHeight);
 
 
     /** Activates input event listeners */
@@ -100,7 +133,7 @@ window.addEventListener('load', function() {
         item.addEventListener("mouseout", function() {
             getSchwifty(cardContainer, cardContainerWidth, cardContainerHeight);
             document.querySelector('#description').style.display = 'none';
-            document.querySelector('#greeting').style.display = 'block';
+            document.querySelector('#punchline').style.display = 'block';
         });
     });
 
@@ -120,7 +153,7 @@ window.addEventListener('load', function() {
         e.stopPropagation();
         getSchwifty(cardContainer, cardContainerWidth, cardContainerHeight);
         document.querySelector('#description').style.display = 'none';
-        document.querySelector('#greeting').style.display = 'block';
+        document.querySelector('#punchline').style.display = 'block';
     });
 
     document.querySelectorAll(".icon").forEach(function(item) {
@@ -133,12 +166,12 @@ window.addEventListener('load', function() {
     document.querySelectorAll(".icon").forEach(function(icon) {
         icon.addEventListener("mousemove", function() {
             document.querySelector('#description').innerHTML = icon.getAttribute('alt');
-            document.querySelector('#greeting').style.display = 'none';
+            document.querySelector('#punchline').style.display = 'none';
             document.querySelector('#description').style.display = 'block';
         });
         icon.addEventListener("mouseout", function() {
             document.querySelector('#description').style.display = 'none';
-            document.querySelector('#greeting').style.display = 'block';
+            document.querySelector('#punchline').style.display = 'block';
         });
     });
 
@@ -159,27 +192,35 @@ function gateway(node, target) {
             gtag('event', 'click', {
                 'event_category': 'mail'
             });
-            navigator.clipboard.writeText("hello@jbbmb.com");
-            SnackBar({
-                message: "Address copied to the clipboard  🤙🏻&nbsp",
-                status: "green",
-                position: "tr",
-                fixed: true,
-                timeout: 6000,
-                dismissible: false
-            });
+            setTimeout(() => {
+                navigator.clipboard.writeText("hello@jbbmb.com")
+                SnackBar({
+                    message: "&nbspAddress copied to the clipboard.&nbsp",
+                    status: "green",
+                    position: "tl",
+                    fixed: true,
+                    timeout: 7000,
+                    dismissible: false
+                });
+              }, 0);
             break;
-        case 3:
+        case 2:
             gtag('event', 'click', {
                 'event_category': 'telegram'
             });
             window.open("https://telegram.me/jbbmb", target);
             break;
-        case 4:
+        case 3:
             gtag('event', 'click', {
                 'event_category': 'pay'
             });
             window.open("https://revolut.me/jbbmb", target);
+            break;
+        case 4:
+            gtag('event', 'click', {
+                'event_category': 'bluesky'
+            });
+            window.open("https://bsky.app/profile/jbbmb.com", target);
             break;
         case 5:
             gtag('event', 'click', {
@@ -189,33 +230,34 @@ function gateway(node, target) {
             break;
         case 6:
             gtag('event', 'click', {
-                'event_category': 'git'
-            });
-            window.open("https://github.com/jbbmb", target);
-            break;
-        case 7:
-            gtag('event', 'click', {
                 'event_category': 'music'
             });
             window.open("https://music.apple.com/playlist/c-re/pl.u-6mo4lN4H4NWedB", target);
             break;
-        case 8:
-            gtag('event', 'click', {
-                'event_category': 'photos'
-            });
-            break;
-        case 9:
+        case 7:
             gtag('event', 'click', {
                 'event_category': 'videos'
             });
             window.open("https://www.youtube.com/@jbbmb", target);
             break;
-        case 10:
+        case 8:
+            gtag('event', 'click', {
+                'event_category': 'bereal'
+            });
+            window.open("https://bere.al/jbbmb", target);
+            break;
+        case 9:
             gtag('event', 'click', {
                 'event_category': 'shortcuts'
             });
             window.open("https://routinehub.co/user/jbbmb", target);
             break;
+        case 10:
+            gtag('event', 'click', {
+                'event_category': 'git'
+            });
+            window.open("https://github.com/jbbmb", target);
+        break;
         case 11:
             gtag('event', 'click', {
                 'event_category': 'cvitae_pdf'
@@ -223,61 +265,52 @@ function gateway(node, target) {
             window.open("https://github.com/jbbmb/jbbmb.github.io/raw/main/static/pdf/Curriculum%20Vitae%20of%20João%20de%20Macedo%20Borges.pdf", target);
             setTimeout(() => {
                 SnackBar({
-                    message: "Résumé downloaded successfully  👍🏻&nbsp",
+                    message: "&nbspFile downloaded successfully.&nbsp",
                     status: "green",
-                    position: "tr",
+                    position: "tl",
                     fixed: true,
-                    timeout: 6000,
+                    timeout: 7000,
                     dismissible: false
                 });
                 document.querySelector('#description').style.display = 'none';
-                document.querySelector('#greeting').style.display = 'block';
+                document.querySelector('#punchline').style.display = 'block';
             }, 500);
             break;
         case 12:
-            window.open("https://github.com/jbbmb/jbbmb.github.io#privacy-policy", target);
+            window.open("https://github.com/jbbmb/jbbmb.github.io/blob/main/LICENSE.md", target);
             break;
         case 13:
-            window.open("https://github.com/jbbmb/jbbmb.github.io/commits/main", target);
+            window.open("https://github.com/jbbmb/jbbmb.github.io/blob/main/README.md", target);
             break;
         case 14:
-            gtag('event', 'click', {
-                'event_category': 'bluesky'
-            });
-            window.open("https://bsky.app/profile/jbbmb.com", target);
-            break;
-        case 15:
-            gtag('event', 'click', {
-                'event_category': 'bereal'
-            });
-            window.open("https://bere.al/jbbmb", target);
+            window.open("http://stats.uptimerobot.com/OXmELf5EPJ", target);
             break;
         case 404:
             setTimeout(() => {
                 SnackBar({
-                    message: "The requested page was not found  👊🏻&nbsp",
+                    message: "&nbspThe requested page was not found.&nbsp",
                     status: "error",
-                    position: "tr",
+                    position: "tl",
                     fixed: true,
-                    timeout: 6000,
+                    timeout: 7000,
                     dismissible: false
                 });
                 document.querySelector('#description').style.display = 'none';
-                document.querySelector('#greeting').style.display = 'block';
+                document.querySelector('#punchline').style.display = 'block';
             }, 500);
             break;
         default:
             setTimeout(() => {
                 SnackBar({
-                    message: "An internal error has occurred  👎🏻&nbsp",
+                    message: "&nbspAn internal error has occurred.&nbsp",
                     status: "error",
-                    position: "tr",
+                    position: "tl",
                     fixed: true,
-                    timeout: 6000,
+                    timeout: 7000,
                     dismissible: false
                 });
                 document.querySelector('#description').style.display = 'none';
-                document.querySelector('#greeting').style.display = 'block';
+                document.querySelector('#punchline').style.display = 'block';
             }, 500);
             break;
     }
@@ -287,28 +320,71 @@ function gateway(node, target) {
 /** Loads the selected route */
 
 function reload(node) {
-    const next = ("https://jbbmb.com#").concat(node.toString());
+    const next = ("https://jbbmb.com?").concat(node.toString());
     window.location.href = next;
 }
 
 
-/** Greets the visitor */
+/** Scrolls to the top */
 
-function getGreeting() {
-    var goodX = [
-            [0, 5, "good night!"],
-            [6, 11, "good morning!"],
-            [12, 19, "good afternoon!"],
-            [20, 23, "good evening!"]
-        ],
-        hr = new Date().getHours();
-    for (var i = 0; i < goodX.length; i++) {
-        if (hr >= goodX[i][0] && hr <= goodX[i][1]) {
-            console.log("So, we're opening Web Inspector now...?");
-            return "Hello, ".concat(goodX[i][2]);
-        }
-    }
+function bopToTheTop() {
+    document.body.scrollTop = 0; // Safari
+    document.documentElement.scrollTop = 0; // Chrome, Firefox, IE and Opera
 }
+
+
+/** Pushes to Google */
+
+function gtag() {
+    dataLayer.push(arguments);
+}
+
+
+/** Scroll Magic for video intro */
+
+const controller = new ScrollMagic.Controller();
+
+// Start Section
+const intro = document.querySelector(".intro");
+const video = intro.querySelector("video");
+const text = intro.querySelector("h1");
+
+// End Section
+const section = document.querySelector("section");
+const end = section.querySelector("h1");
+
+// Scene
+let scene = new ScrollMagic.Scene({
+        duration: 8000,
+        triggerElement: intro,
+        triggerHook: 0
+    })
+    .setPin(intro)
+    .addTo(controller);
+
+// Text Animation
+const textAnim = TweenMax.fromTo(text, 3, { opacity: 0.9 }, { opacity: 0 });
+let scene2 = new ScrollMagic.Scene({
+        duration: 3000,
+        triggerElement: intro,
+        triggerHook: 0
+    })
+    .setTween(textAnim)
+    .addTo(controller);
+
+// Video Animation
+let accelamount = 0.1;
+let scrollpos = 0;
+let delay = 0;
+scene.on("update", e => {
+    scrollpos = e.scrollPos / 1000;
+});
+setInterval(() => {
+    requestAnimationFrame(() => {
+        delay += (scrollpos - delay) * accelamount;
+        video.currentTime = delay;
+    })
+}, 60);
 
 
 /** TextScramble */
@@ -365,8 +441,4 @@ class TextScramble {
     randomChar() {
         return this.chars[Math.floor(Math.random() * this.chars.length)]
     }
-}
-
-function gtag() {
-    dataLayer.push(arguments);
 }
